e26a6c891f7382ddf9704992583e09a2f0f99fc5
ArsonXY <arson.xy.business@gmail.com>
2017-01-19 21:19:17 -0500

    Put commands for path1 and path2 in their own functions so we can easily move them around in the loop() function. Added function and file headers due to it being a requirement in the lab manual.

--- Lab 1/lab1.ino
+++ Lab 1/lab1.ino
@@ -1,3 +1,14 @@
+/**
+* Ryan Fitzgerald (7233237)
+* Cody McCoshen (7208960)
+* SEG4145
+* Laboratory 1
+* lab1.ino
+* This file is responsible for making the robot perform Path 1 and
+* Path 2 detailed in the Lab manual for Laboratory 1. The robot
+* will print the command its performing on its LCD screen.
+*/
+
 #include <SoftwareSerial.h>
 #include <stdio.h>
 
@@ -14,7 +25,6 @@ int flashing;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
     // Assign all required pins
     pinMode(13, OUTPUT); // LED
     pinMode(45, OUTPUT); // Left Motor
@@ -55,14 +65,142 @@ void setup() {
 
     // Clear LCD
     clearLCD();
-
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
     // --- Path 1 ---
+    doPath1();
 
+    // --- Path 2 ---
+    doPath2();
+}
+
+/**
+* Clears all characters from the LCD screen.
+* @name clearLCD
+* @return (void)
+*/
+void clearLCD() {
+    LCD.write(0xFE);  // Put the LCD screen in command mode.
+    LCD.write(0x01);  // Clear the LCD screen.
+    delay(10);
+}
+
+/**
+* Prints a message to the LCD screen.
+* @name printMessage
+* @param msg the message that will be printed
+* @return (void)
+*/
+void printMessage(char* msg) {
+    // Clear the LCD
+    clearLCD();
+
+    // Put the LCD screen in command mode.
+    LCD.write(0xFE);
+
+    // Set cursor to first row, first column
+    LCD.write(0 + 0*64 + 128);
+
+    // Print the message
+    LCD.print(msg);
+    delay(10);
+}
+
+/**
+* Flashes the light every second for num amount of time.
+* @name flashLight
+* @param num number of seconds that the light will be flashing
+* @return (void)
+*/
+void flashLight(int num) {
+    for (flashing = 0; flashing < num; flashing++) {
+        // Wait for 1s
+        delay(1000);
+
+        // turn the LED on (HIGH is the voltage level)
+        digitalWrite(13, HIGH);
+
+        // Wait for 0.01s
+        delay(10);
+
+        // turn the LED on (HIGH is the voltage level)
+        digitalWrite(13, LOW);
+    }
+}
+
+/**
+* Stops the robot. A delay is provided to determine how long
+* the robot will remain in this state.
+* @name stopMotor
+* @param duration number of milliseconds that the robot will remain in this state
+* @return (void)
+*/
+void stopMotor(int duration) {
+    LEFT_STOP();
+    RIGHT_STOP();
+    delay(duration);
+}
+
+/**
+* Moves the robot forward. A delay is provided to determine how long
+* the robot will remain in this state.
+* @name moveForward
+* @param duration number of milliseconds that the robot will remain in this state
+* @return (void)
+*/
+void moveForward(int duration) {
+    LEFT_FORWARD();
+    RIGHT_FORWARD();
+    delay(duration);
+}
+
+/**
+* Moves the robot backward. A delay is provided to determine how long
+* the robot will remain in this state.
+* @name moveBackward
+* @param duration number of milliseconds that the robot will remain in this state
+* @return (void)
+*/
+void moveBackward(int duration) {
+    LEFT_BACKWARD();
+    RIGHT_BACKWARD();
+    delay(duration);
+}
+
+/**
+* Turns the robot left. A delay is provided to determine how long
+* the robot will remain in this state.
+* @name turnLeft
+* @param duration number of milliseconds that the robot will remain in this state
+* @return (void)
+*/
+void turnLeft(int duration) {
+    LEFT_BACKWARD();
+    RIGHT_FORWARD();
+    delay(duration);
+}
+
+/**
+* Turns the robot right. A delay is provided to determine how long
+* the robot will remain in this state.
+* @name turnRight
+* @param duration number of milliseconds that the robot will remain in this state
+* @return (void)
+*/
+void turnRight(int duration) {
+    LEFT_FORWARD();
+    RIGHT_BACKWARD();
+    delay(duration);
+}
+
+/**
+* Provides the commands to make the robot perform Path 1.
+* @name doPath1
+* @return (void)
+*/
+void doPath1(){
     // Move straight
     printMessage("Moving Forward");
     moveForward(1000);
@@ -102,9 +240,14 @@ void loop() {
     // Stop motor for 5s
     printMessage("Finish Path 1");
     stopMotor(5000);
+}
 
-    // --- Path 2 ---
-
+/**
+* Provides the commands to make the robot perform Path 2.
+* @name doPath2
+* @return (void)
+*/
+void doPath2(){
     // Move straight
     printMessage("Moving Forward");
     moveForward(1000);
@@ -144,80 +287,4 @@ void loop() {
     // Stop motor for 5s
     printMessage("Finish Path 2");
     stopMotor(5000);
-
-}
-
-// Clear LCD Screen
-void clearLCD() {
-    LCD.write(0xFE);  // Put the LCD screen in command mode.
-    LCD.write(0x01);  // Clear the LCD screen.
-    delay(10);
-}
-
-// Print a message
-void printMessage(char* msg) {
-    // Clear the LCD
-    clearLCD();
-
-    // Put the LCD screen in command mode.
-    LCD.write(0xFE);
-
-    // Set cursor to first row, first column
-    LCD.write(0 + 0*64 + 128);
-
-    // Print the message
-    LCD.print(msg);
-    delay(10);
-}
-
-// Flash light
-void flashLight(int num) {
-    for (flashing = 0; flashing < num; flashing++) {
-        // Wait for 1s
-        delay(1000);
-
-        // turn the LED on (HIGH is the voltage level)
-        digitalWrite(13, HIGH);
-
-        // Wait for 0.01s
-        delay(10);
-
-        // turn the LED on (HIGH is the voltage level)
-        digitalWrite(13, LOW);
-    }
-}
-
-// Stop the robot
-void stopMotor(int duration) {
-    LEFT_STOP();
-    RIGHT_STOP();
-    delay(duration);
-}
-
-// Move forward
-void moveForward(int duration) {
-    LEFT_FORWARD();
-    RIGHT_FORWARD();
-    delay(duration);
-}
-
-// Move backwards
-void moveBackward(int duration) {
-    LEFT_BACKWARD();
-    RIGHT_BACKWARD();
-    delay(duration);
-}
-
-// Turn left
-void turnLeft(int duration) {
-    LEFT_BACKWARD();
-    RIGHT_FORWARD();
-    delay(duration);
-}
-
-// Turn right
-void turnRight(int duration) {
-    LEFT_FORWARD();
-    RIGHT_BACKWARD();
-    delay(duration);
 }
