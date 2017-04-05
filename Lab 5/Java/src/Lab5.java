6832aac8953b2d1ef58fccae13631f1124b792ff
Ryan Fitzgerald <rfitz042@uottawa.ca>
2017-04-04 21:26:51 -0400

    Fixed repo

--- Lab 5/Java/src/Lab5.java
+++ Lab 5/Java/src/Lab5.java
@@ -1,170 +1,140 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Lab5 {
-	
+
 	private static final int PORT = 9876;
+	public static JFrame frame;
+	public static JLabel operation;
+	public static JLabel timestamp;
+	public static int command = -1;
+	public static int additionalCommand;
 
 	/*
-	 * Main method to start the ServerSocket, Listen for Clients,
-	 * and handle communication between Sender / Receiver
-	 */
+     * Main method to start the ServerSocket, Listen for Clients,
+     * and handle communication between Sender / Receiver
+     */
 	public static void main(String args[]) throws IOException {
-		
+
 		// Reading from System.in
-		Scanner reader = new Scanner(System.in);  
-		
+		Scanner reader = new Scanner(System.in);
+
 		try (
-			
-			// Create the server socket for the Receiver
-			ServerSocket receiverSocket = new ServerSocket(PORT);
-			
-			// Create the socket for the sender by accepting connections
-			Socket senderSocket = receiverSocket.accept();
-				
-			// Create output stream to sender
-			PrintWriter out = new PrintWriter(senderSocket.getOutputStream(), true);
-			
-			// Create input stream from sender
-			BufferedReader in = new BufferedReader(new InputStreamReader(senderSocket.getInputStream()));
-			
+
+				// Create the server socket for the Receiver
+				ServerSocket receiverSocket = new ServerSocket(PORT);
+
+				// Create the socket for the sender by accepting connections
+				Socket senderSocket = receiverSocket.accept();
+
+				// Create output stream to sender
+				PrintWriter out = new PrintWriter(senderSocket.getOutputStream(), true);
+
+				// Create input stream from sender
+				BufferedReader in = new BufferedReader(new InputStreamReader(senderSocket.getInputStream()));
+
 		) {
-			
+
 			// Create string to store input
 			String inputLine;
-			
+
 			// Print port information
 			System.out.println("Listening on port " + PORT);
- 
-            // While input is received
-            while ((inputLine = in.readLine()) != null) {
 
-            	// Store message being sent to robot
-            	String message;
+			frame = new JFrame("SEG4145 Lab 5");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.getContentPane().setLayout(new GridLayout(5, 3));
 
-            	try {
-        			
-        			// Print possible commands
-        			System.out.print("Enter the correct number to select an operation: \n"
-	            					+ "1 - Move the robot forward.\n"
-	            					+ "2 - Move the robot backward.\n"
-	            					+ "3 - Rotate the robot clockwise.\n"
-	            					+ "4 - Rotate the robot counter clockwise.\n"
-	            					+ "5 - Read the distance to the nearest object.\n"
-	            					+ "6 - Read temperature values.\n"
-	            					+ "7 - Quit\n");
-        			
-        			// Scans the next token of the input as an int.
-        			int command = reader.nextInt();
-        			
-        			// Check for incorrect input
-        			if (command == 7) {
-        				System.out.println("Quit selected, quiting now.");
-        				message = "7";
-        			} else if (command > 7 || command < 1) {
-        				System.out.println("Incorrect input, ensure int from 1 to 7 is used.");
-        				break;
-        			} else {
-						// Pass command chosen to function which further prompts user
-						// and stores result (Format: {command, additional})
-						message = processInput(command);
-					}
+			// Create first row
+			frame.add(new JLabel("", JLabel.CENTER));
+			frame.add(new JLabel("Select an Operation:", JLabel.CENTER));
+			frame.add(new JLabel("", JLabel.CENTER));
 
-					// Send the result to robot
-					out.println(message);
-					
-				} catch (NullPointerException e) {
-					System.out.println("Nothing entered, closing connection");
-					System.exit(-1);
-				}
-                
-            }
-            
-            // Close scanner
-			reader.close();
-			
+			// Create second row
+			createButton("Move Forward", 1, frame, out);
+			createButton("Move Backward", 2, frame, out);
+			createButton("Rotate Clockwise", 3, frame, out);
+
+			// Create third row
+			createButton("Rotate Counter Clockwise", 4, frame, out);
+			createButton("Read Distance", 5, frame, out);
+			createButton("Read Temperature", 6, frame, out);
+
+			// Create fourth row
+			frame.add(new JLabel("", JLabel.CENTER));
+			operation = new JLabel("Last Operation: ", JLabel.CENTER);
+			frame.add(operation);
+			frame.add(new JLabel("", JLabel.CENTER));
+
+			// Create fifth row
+			frame.add(new JLabel("", JLabel.CENTER));
+			timestamp = new JLabel("Timestamp: ", JLabel.CENTER);
+			frame.add(timestamp);
+			frame.add(new JLabel("", JLabel.CENTER));
+
+			frame.pack();
+			frame.setVisible(true);
+
+			// While input is received
+			while ((inputLine = in.readLine()) != null) {
+
+				// Continue loop while robot is connected
+
+			}
+
 		} catch (IOException e) {
 			// Could not listen on a given port
 			System.err.println("Could not listen on port " + PORT);
-            System.exit(-1);
-		} 	    
-		
-	}
-	
-	/*
-	 * Method that takes user entered command as input
-	 * and prompts the user for any additional required information
-	 * in order to be sent to the robot
-	 */
-	private static String processInput(int command) {
-		
-		// Reading from System.in
-		Scanner reader = new Scanner(System.in); 
-		
-		int[] result = new int[2];
-		
-		// Store command as first entry
-		result[0] = command;
-		
-		// Store any readings
-		int additionalCommand;
-		
-		if (command == 1 || command == 2) { // Move forward or backward
-			// Prompt user for distance
-			System.out.println("Enter the distance in cm: ");
-			
-			// Read user input
-			additionalCommand = reader.nextInt();
-
-			if (additionalCommand > 20 || additionalCommand < 1) {
-				System.out.println("Incorrect value.");
-			} else {
-				// Store result
-				result[1] = additionalCommand;
-			}
-
-			// Store result
-			result[1] = additionalCommand;
-		} else if (command == 3 || command == 4) { // Rotate clockwise or counter-clockwise
-			// Prompt user for degrees
-			System.out.println("Enter the degrees to turn: ");
-			
-			// Read user input
-			additionalCommand = reader.nextInt();
-
-			if (additionalCommand > 359 || additionalCommand < 1) {
-				System.out.println("Incorrect value.");
-			} else {
-				// Store result
-				result[1] = additionalCommand;
-			}
-		} else if (command == 5) { // Read sonar
-			// Notify user of sonar value being read
-			System.out.println("Sonar will be read.");
-			
-			// Return arbitrary int
-			result[1] = 0;
-		} else if (command == 6) { // Read temperature
-			// Prompt for temperature type
-			System.out.print("Enter temperature type you wish to receive: \n"
-        					+ "1 - Celcius.\n"
-        					+ "2 - Fahrenheit.\n");
-			
-			// Read user input
-			additionalCommand = reader.nextInt();
-			
-			// Incorrect value
-			if (additionalCommand != 2 && additionalCommand != 1) {
-				System.out.println("Incorrect value.");
-			} else {
-				// Store result
-				result[1] = additionalCommand;
-			}
+			System.exit(-1);
 		}
 
-		// Return the resulting concatenated string
-		return "" + result[0] + result[1] + "\n";
 	}
-	
+
+	/*
+     * Method used to create UI buttons and add action listeners
+     * which in turn send input back to the robot when clicked
+     */
+	private static void createButton(String text, int val, JFrame frame, PrintWriter out) {
+		// Create button
+		JButton btn = new JButton(text);
+
+		// Add listener
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				// Update command
+				command = val;
+
+				// Get additional command information based on command
+				if (command == 1 || command == 2) {
+					String distance = JOptionPane.showInputDialog("Enter Distance (0 to 20):");
+					additionalCommand = Integer.parseInt(distance);
+				} else if (command == 3 || command == 4) {
+					String degrees = JOptionPane.showInputDialog("Enter Degrees (0 to 359):");
+					additionalCommand = Integer.parseInt(degrees);
+				} else if (command == 6) {
+					String tempType = JOptionPane.showInputDialog("Enter Temperature Type (1 - Celsius, 2 - Fahrenheit):");
+					additionalCommand = Integer.parseInt(tempType);
+				} else { // If reading distance
+					additionalCommand = 0;
+				}
+
+				// Update UI
+				operation.setText("Last Operation: " + command);
+				timestamp.setText("Timestamp: " + new SimpleDateFormat("dd/mm/yy HH:mm").format(new Date()));
+
+				// Send Result to user
+				out.println("" + command + additionalCommand + "\n");
+			}
+		});
+
+		// Add to frame
+		frame.add(btn);
+	}
+
 }
